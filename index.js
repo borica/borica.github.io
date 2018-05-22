077f176326c00003ef59de900d16503da3ff6637
Tiago Ronaldo Boriça <tiago.borica@sigma.com.br>
2018-05-22 14:03:16 -0300

    JavaScript Logic to work out with logo Animations

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-/*
-    $.getJSON( "navbar.json", function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-            items.push( "<li id='" + key + "'>" + val + "</li>" );
-        });
-
-        $( "<ul/>", {
-            "class": "my-new-list",
-            html: items.join( "" )
-        }).appendTo( "body" );
-    });
-*/
-
 /* Animated Text */
 document.addEventListener('DOMContentLoaded',function(event){
 
@@ -54,7 +40,7 @@ document.addEventListener('DOMContentLoaded',function(event){
     }
   }
   // start the text animation
-  StartTextAnimation(0);
+  //StartTextAnimation(0);
 });
 
 /* SweetScroll Settings */
@@ -66,3 +52,38 @@ var scroller = new SweetScroll({
 window.sr = ScrollReveal();
 sr.reveal('.h1-reveal');
 sr.reveal('.p-reveal');
+sr.reveal('.div-reveal');
+
+function animationTrigger(event){
+  /* Declaring Variables */
+  let nav = document.getElementById("navbar");
+  let navElements = nav.getElementsByTagName('li');
+  let liElements = [];
+  let validationHelper = [];
+
+  /* Pushing elements to the array */
+  $.each(navElements, function(index, value){
+    liElements.push(value.getElementsByTagName('a'));
+  })
+
+  /* Iterating over elements */
+  $.each(liElements, function(indexLi, valueLi){
+    $.each(valueLi, function(indexA, valueA){
+        $.each(valueA.classList, function(indexClass, valueClass){
+          if(valueA.id == "linkHome" && valueClass == "active"){
+            validationHelper.push('true');
+          }else{
+            validationHelper.push('false');
+          }
+        });
+    });
+  });
+  if(validationHelper.includes('true')){
+    //Make the animation for the 'TB' Appears
+  }
+  //Figure out a way to validate if the full logo should animate or just keep frozen
+}
+
+$(window).on('activate.bs.scrollspy', function (event) {
+  animationTrigger(event);
+})
