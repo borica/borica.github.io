20fe69c402349b4e5a45389672e3832c0b97464d
Tiago Ronaldo Boriça <tiago.borica@sigma.com.br>
2018-05-23 12:05:04 -0300

    Managing Animations for the Logo

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
 /* Animated Text */
-document.addEventListener('DOMContentLoaded',function(event){
 
-  // array with texts to type in typewriter
-  var dataText = [ "Tiago Boriça"];
+ // array with texts to type in typewriter
+ let dataText = ["Tiago Boriça", "TB"];
 
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    if (i < (text.length)) {
-      // add next character to h1
-     document.getElementById("animatedText").innerHTML = text.substring(0, i+1) +'<span class="animatedCaret" aria-hidden="true"></span>';
+ // type one text in the typwriter
+ // keeps calling itself until the text is finished
+ function typeWriter(text, i, fnCallback) {
+   // chekc if text isn't finished yet
+   if (i < (text.length)) {
+     // add next character to h1
+    document.getElementById("animatedText").innerHTML = text.substring(0, i+1) +'<span class="animatedCaret" aria-hidden="true"></span>';
 
-      // wait for a while and call this function again for next character
-      setTimeout(function() {
-        typeWriter(text, i + 1, fnCallback)
-      }, 100);
+     // wait for a while and call this function again for next character
+     setTimeout(function() {
+       typeWriter(text, i + 1, fnCallback)
+     }, 100);
+   }
+   // text finished, call callback if there is a callback function
+   else if (typeof fnCallback == 'function') {
+     // call callback after timeout
+     setTimeout(fnCallback, 700);
+   }
+ }
+ // start a typewriter animation for a text in the dataText array
+  function StartTextAnimation(i) {
+    $("#animatedText").fadeIn("slow");
+    if (typeof dataText[i] == 'undefined'){
+       setTimeout(function() {
+         StartTextAnimation(0);
+       }, 20000);
     }
-    // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
-      // call callback after timeout
-      setTimeout(fnCallback, 700);
-    }
-  }
-  // start a typewriter animation for a text in the dataText array
-   function StartTextAnimation(i) {
-     if (typeof dataText[i] == 'undefined'){
-        setTimeout(function() {
-          StartTextAnimation(0);
-        }, 20000);
-     }
-     // check if dataText[i] exists
-    if (i < dataText[i].length) {
-      // text exists! start typewriter animation
-     typeWriter(dataText[i], 0, function(){
-       // after callback (and whole text has been animated), start next text
-       /*StartTextAnimation(i + 1);*/
-     });
-    }
-  }
-  // start the text animation
-  //StartTextAnimation(0);
-});
+    // check if dataText[i] exists
+   if (i < dataText[i].length) {
+     // text exists! start typewriter animation
+    typeWriter(dataText[i], 0, function(){
+      // after callback (and whole text has been animated), start next text
+      /*StartTextAnimation(i + 1);*/
+    });
+   }
+ }
+ // start the text animation
+ 
 
 /* SweetScroll Settings */
-var scroller = new SweetScroll({
+let scroller = new SweetScroll({
     trigger: 'a[href^="#"]',
     offset: -56
 });
@@ -80,8 +79,17 @@ function animationTrigger(event){
   });
   if(validationHelper.includes('true')){
     //Make the animation for the 'TB' Appears
+    $("#animatedText").fadeOut("slow", function(){
+      StartTextAnimation(1);
+      $("#animatedText").fadeIn("slow");
+    });
+  } else {
+    //Figure out a way to validate if the full logo should animate or just keep frozen
+    $("#animatedText").fadeOut("slow", function(){
+      StartTextAnimation(0);
+      $("#animatedText").fadeIn("slow");
+    });
   }
-  //Figure out a way to validate if the full logo should animate or just keep frozen
 }
 
 $(window).on('activate.bs.scrollspy', function (event) {
