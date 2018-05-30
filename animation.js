689c98b3a336b5dc609963803a4277b36c024288
Tiago Boriça <tiago.borica@gmail.com>
2018-05-29 21:07:33 -0300

    SVG Files, Started logic to animated SVG background

--- animation.js
+++ animation.js
@@ -3,7 +3,30 @@ $('document').ready(function() {
 });
 
 function animate() {
-    let ball = document.createElement('div');
-    ball.addClass("ball");
-    /* $('.ball').animate({marginTop : "-500px"}, 'slow'); */
+    //Selecting The Landing Jumbo
+    let jumbo = document.getElementById("top");
+    
+    //Container for the SVG
+    let div = document.createElement("div");
+    
+    //Creating SVG ELement
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+    //Creating the USE Element
+    /* let use = document.createElementNS("http://www.w3.org/2000/svg", "use"); */
+    
+    //Setting the USE Attr
+    svg.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "assets/web-programming-icons/" + generateRandom(1, 30) + ".svg#Capa1");
+    
+    /* svg.appendChild(use); */
+    
+    //Adding Class 
+    div.classList.add('animationContainer');
+    div.appendChild(svg);
+    jumbo.appendChild(div);
+    /* $('.animationContainer').animate({marginTop : "-500px", marginLeft : '500px'}, 3700); */
+}
+
+function generateRandom(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
