9ed43ec03a83e81f0f36ca75468e98a608492e08
Tiago Borica <tiago.borica@gmail.com>
2021-01-26 12:41:57 -0300

    New mods..

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 $(() => {
     $('.start-hidden').hide();
+
     $('.card-header').click((eventHandle)=>{
         let cardBody = "";
         let children = eventHandle.currentTarget.parentElement.children;
+        
         for(let i = 0; i < children.length; i++) {
             if(eventHandle.currentTarget.parentElement.children[i].className.indexOf("card-body") >= 0) {
                 cardBody = eventHandle.currentTarget.parentElement.children[i];
